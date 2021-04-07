1b1569dd0c0e5be7b2fa3f8104c47c60cc4a50c1
Charles Escott <charlescescott@gmail.com>
2021-04-06 17:40:44 -0700

    Starting Volley Branch
    Added volley to gradle dependencies
    Added buttons to test connectivity

--- app/src/main/java/com/example/iot_lab4/MainActivity.java
+++ app/src/main/java/com/example/iot_lab4/MainActivity.java
@@ -9,14 +9,14 @@ import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button speechButton;
     EditText speechText;
+    Button button_on;
+    Button button_off;
 
     private static final int RECOGNIZER_RESULT = 1;
 
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         speechButton = findViewById(R.id.speechButton);
         speechText = findViewById(R.id.speechText);
+        button_on = findViewById(R.id.button_on);
+        button_off = findViewById(R.id.button_off);
 
         speechButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +37,20 @@ public class MainActivity extends AppCompatActivity {
                 speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
                 speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech to Text");
                 startActivityForResult(speechIntent, RECOGNIZER_RESULT);
+            }
+        });
+
+        button_on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        button_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
